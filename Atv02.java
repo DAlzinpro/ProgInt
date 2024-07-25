9d4fd2e9038cb6d51b78d63bf3866ab82ef93654
DAlzinpro <101902234+DAlzinpro@users.noreply.github.com>
2024-07-25 07:49:34 -0300

    Create Atv02.java

---
+++ Atv02.java
@@ -0,0 +1,14 @@
+public class Atv02{
+    public static void main(String[] args){
+        int numeroInt = 20;
+        double numeroDouble = 20.20;
+        long numeroLong =2000;
+        float numeroFloat = 20.20f;
+        char crt = 74;
+        boolean bool = false;
+        byte numeroByte = 20;
+        short numeroShort = 20;
+        
+        System.out.println("inteiro:\n"+numeroInt+"\t numero double: "+numeroDouble+"\\numero Long:"+numeroLong+"numero float"+numeroFloat+"char: " + crt +"boolean: "+bool+"numero byte"+numeroByte+"numero Short"+numeroShort);
+    }
+}
